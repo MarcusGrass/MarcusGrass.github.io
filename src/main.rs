3d73e91263fabee0929661077399dbde16d14279
gramar <marcus.grass@gmail.com>
2022-12-05 18:55:19 +0100

    rewrite js

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,233 @@
-use std::fmt::{Display, Formatter};
+use std::fmt::Write;
+use std::path::{Path, PathBuf};
 
-use yew::{function_component, html, Html};
-use yew_router::{BrowserRouter, Routable, Switch};
+use pulldown_cmark::Parser;
 
-use crate::menu::Sidebar;
-
-mod menu;
-mod pages;
-
-#[derive(Debug, Copy, Clone, Eq, PartialEq, Routable)]
-pub enum Location {
-    #[at("/")]
-    Home,
-    #[at("/pgwm03")]
-    Pgwm03,
-    #[at("/test")]
-    Test,
-}
-
-impl Display for Location {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Location::Home => f.write_str("Home"),
-            Location::Pgwm03 => f.write_str("Pgwm 03"),
-            Location::Test => f.write_str("Test"),
-        }
-    }
-}
-
-#[function_component]
-fn App() -> Html {
-    html! {
-        html! {
-            <BrowserRouter>
-                <Switch<Location> render={render_content} />
-            </BrowserRouter>
-        }
-    }
-}
-
-fn render_content(location: Location) -> Html {
-    let content = match location {
-        Location::Home => pages::home::page_html(),
-        Location::Pgwm03 => pages::projects::pgwm03::page_html(),
-        Location::Test => pages::projects::test::page_html(),
-    };
-    html! {
-        <>
-        <Sidebar />
-        <div id="content">
-            {content}
-        </div>
-        </>
-    }
-}
+const HOME: LocationInfo = LocationInfo::new("/", "Home");
+const HOME_LABEL: &str = "Home";
+const NAV: LocationInfo = LocationInfo::new("/table-of-contents", "Nav");
+const NAV_LABEL: &str = "Table of contents";
+const NOT_FOUND: LocationInfo = LocationInfo::new("/not-found", "NotFound");
+const LOCATIONS: [LocationInfo; 5] = [
+    HOME,
+    NAV,
+    NOT_FOUND,
+    LocationInfo::new("/test", "Test"),
+    LocationInfo::new("/pgwm03", "Pgwm03"),
+];
 
 fn main() {
-    yew::Renderer::<App>::new().render();
+    let workspace = std::env::current_dir().expect("Failed to get workspace from env");
+    let ws_path = PathBuf::from(&workspace);
+    let md_pages = ws_path.join("pages");
+    let md_content = recurse_convert_pages(&md_pages, &PathBuf::new()).unwrap();
+    let location_content = location_file_content();
+    let mut total_content = location_content.into_bytes();
+    for content in md_content {
+        total_content.extend(content.into_bytes());
+    }
+    total_content.extend(create_js().into_bytes());
+    std::fs::write(
+        ws_path.join("static").join("generated.js"),
+        &total_content,
+    )
+    .unwrap();
+    copy_minified(&ws_path).unwrap();
+}
+
+fn create_js() -> String {
+    let mut raw = String::new();
+    raw.push_str("function render(location) {\n");
+    let _ = raw.write_fmt(format_args!("\
+        \tif (location === Location.{}.path) {{\n\
+        {}\
+        \t}} else if (location === Location.{}.path) {{\n\
+        {}\
+        \t}} \
+        ", HOME.tag_name.to_uppercase(), create_inner_render(HOME.tag_name, Some((NAV_LABEL, NAV.path_name))),
+        NAV.tag_name.to_uppercase(), create_inner_render(NAV.tag_name, Some((HOME_LABEL, HOME.path_name)))
+    ));
+    for location in LOCATIONS.into_iter().skip(3) {
+        let _ = raw.write_fmt(format_args!("\
+            else if (location === Location.{}.path) {{\n\
+            {}\
+            \t}} \
+        ", location.tag_name.to_uppercase(), create_inner_render(location.tag_name, None)));
+    }
+    let _ = raw.write_fmt(format_args!("\
+        else {{\n\
+        {}\
+        \t}}\
+    ", create_inner_render(NOT_FOUND.tag_name, None)));
+    raw.push_str(r#"
+}
+function create_nav_button(label, link) {
+    return "<div><button class=\"menu-item\" onclick=NAVIGATION.navigate(\"" + link + "\")>" + label + "</button></div>";
+}
+
+class Navigation {
+    constructor(location) {
+        this.location = location;
+    }
+
+    navigate(location) {
+        if (location !== this.location) {
+            window.history.pushState({"pageTitle": location}, "", location);
+            render(location)
+        }
+    }
+}
+let cur = window.location.pathname.split("/").pop();
+let NAVIGATION = new Navigation(cur);
+NAVIGATION.navigate(cur);
+    "#);
+    raw
+}
+
+fn create_inner_render(tag_name: &str, has_special_nav: Option<(&str, &str)>) -> String {
+    let mut base = "\t\tdocument.getElementById(\"menu\")\n".to_string();
+    if let Some((nav_label, nav_link)) = has_special_nav {
+        let _ = base.write_fmt(format_args!("\t\t\t.innerHTML = create_nav_button(\"{}\", \"{}\");\n", nav_label, nav_link));
+    } else {
+        let _ = base.write_fmt(format_args!("\t\t\t.innerHTML = create_nav_button(\"{}\", \"{}\") + create_nav_button(\"{}\", \"{}\");\n", HOME_LABEL, HOME.path_name, NAV_LABEL, NAV.path_name));
+    }
+    let _ = base.write_fmt(format_args!("\t\tdocument.getElementById(\"content\")\n\
+    \t\t\t.innerHTML = {}_HTML;\n", tag_name.to_uppercase()));
+    base
+}
+
+fn recurse_convert_pages(
+    md_root: &Path,
+    root_offset: &Path,
+) -> Result<Vec<String>, String> {
+    let search = md_root.join(root_offset);
+    let mut files_here = vec![];
+    for entry in
+        std::fs::read_dir(&search).map_err(|e| format!("Failed to read {md_root:?} {e}"))?
+    {
+        let entry = entry.map_err(|e| format!("Failed to read entry from {md_root:?} {e}"))?;
+        let metadata = entry
+            .metadata()
+            .map_err(|e| format!("Failed to get metadata for entry {entry:?} {e}"))?;
+        let path = entry.path();
+        let file_name = path.file_name().unwrap();
+        let new_offset = root_offset.join(file_name);
+        if metadata.is_dir() {
+            let sub_modules = recurse_convert_pages(md_root, &new_offset)?;
+            files_here.extend(sub_modules.into_iter());
+        } else if metadata.is_file() {
+            let raw = std::fs::read_to_string(&path)
+                .map_err(|e| format!("Failed to read markdown from {path:?} {e}"))?;
+            let out_name = file_name.to_str().unwrap();
+            let (name, _md) = out_name.split_once('.').unwrap();
+            let rust_content = format_page_mod(&raw, name);
+            files_here.push(rust_content);
+        }
+    }
+    Ok(files_here)
+}
+
+#[derive(Copy, Clone, Debug)]
+struct LocationInfo {
+    path_name: &'static str,
+    tag_name: &'static str,
+}
+
+impl LocationInfo {
+    const fn new(path_name: &'static str, tag_name: &'static str) -> Self {
+        Self {
+            path_name,
+            tag_name,
+        }
+    }
+}
+
+
+fn location_file_content() -> String {
+    let mut content = "\
+    // Generated code, don't touch\n\
+    const Location = Object.freeze({\n\
+    "
+    .to_string();
+    for location in LOCATIONS {
+        let _ = content.write_fmt(format_args!(
+            "\t{}: {{\"path\": \"{}\", \"name\": \"{}\"}},\n",
+            location.tag_name.to_uppercase(), location.path_name, location.tag_name
+        ));
+    }
+    content.push_str("});\n");
+    content
+}
+
+fn format_page_mod(md_data: &str, name: &str) -> String {
+    let parser = Parser::new(md_data);
+    let cap = md_data.len() * 2;
+    let mut html_output: String = String::with_capacity(cap);
+    pulldown_cmark::html::push_html(&mut html_output, parser);
+    let mut macro_html = html_output;
+    for location in LOCATIONS {
+        let replace_match = format!("href=\"{}\"", location.path_name);
+        while let Some(_found) = macro_html.find(&replace_match) {
+            macro_html =
+                macro_html.replace(&replace_match, &to_nav_link_fn(location.path_name));
+        }
+    }
+    let js_content = format!(
+        "\
+        /// Generated code, don't touch.\n\
+        const {}_HTML = String.raw`<div class=\"markdown-body\">{}</div>`\n\
+        ", name.to_uppercase(), macro_html
+    );
+    js_content
+}
+
+fn to_nav_link_fn(location: &str) -> String {
+    format!(
+        "class=\"self-link\" onclick=NAVIGATION.navigate(\"{location}\")"
+    )
+}
+
+fn copy_minified(ws: &Path) -> Result<(), String> {
+    let dist = ws.join("dist");
+    std::fs::create_dir_all(&dist)
+        .map_err(|e| format!("Failed to create dist dir {dist:?} {e}"))?;
+    let dist_static = dist.join("static");
+    std::fs::create_dir_all(&dist_static)
+        .map_err(|e| format!("Failed to create dist static dir {dist_static:?} {e}"))?;
+    let src = ws.join("index.html");
+    let dest = dist.join("index.html");
+    std::fs::copy(&src, &dest)
+        .map_err(|e| format!("Failed to copy {:?} to {:?} {e}", src, dest))?;
+    let static_dir = ws.join("static");
+    for entry in std::fs::read_dir(&static_dir).map_err(|e| format!("Failed to read dir {:?} {e}", static_dir))? {
+        let entry = entry.map_err(|e| format!("Failed to read entry at {:?} {e}", static_dir))?;
+        let path = entry.path();
+        let metadata = entry.metadata().map_err(|e| format!("Failed to read metadata for {path:?} {e}"))?;
+        if !metadata.is_file() {
+            return Err(format!("Only regular files allowed in static dir {:?} found something else at {path:?}", static_dir));
+        }
+        let ext = path.extension().ok_or_else(|| format!("Found file without extension in static dir {:?} {path:?}", static_dir))?;
+        let ext_str = ext.to_str().ok_or_else(|| format!("Found non utf8 file extension {ext:?}"))?;
+        let content = std::fs::read_to_string(&path)
+            .map_err(|e| format!("Failed to read content for {path:?} {e}"))?;
+        let minified = match ext_str {
+            "css" => {
+                minifier::css::minify(&content).map_err(|e| format!("Failed to minify {path:?} {e}"))?
+                    .to_string()
+            }
+            "js" => {
+                // TODO: Js minify doesn't work with String.raw` gotta create an issue about that and publish this as an example
+                //minifier::js::minify(&content)
+                //    .to_string()
+                content
+            }
+            _ => return Err(format!("Only .js and .css files allowed in static dir {:?} found {path:?}", static_dir)),
+        };
+        let file_name = path.file_name().unwrap();
+        let out = dist_static.join(file_name);
+        std::fs::write(&out, minified)
+            .map_err(|e| format!("Failed to write minified content to {out:?} {e}"))?
+
+    }
+    Ok(())
 }
--- src/pages/home.rs
+++
@@ -1,9 +0,0 @@
-const RAW_HTML: &str = include_str!("Home.html");
-
-pub fn page_html() -> yew::Html {
-	let div = gloo_utils::document().create_element("div").unwrap();
-	div.set_inner_html(RAW_HTML);
-	div.set_class_name("markdown-body");
-	yew::Html::VRef(div.into())
-}
-
